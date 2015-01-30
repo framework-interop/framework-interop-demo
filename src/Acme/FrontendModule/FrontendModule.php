5b5127c71070c0f28f60c2f1eedfdd955ae9c239
David Négrier <d.negrier@thecodingmachine.com>
2015-01-30 16:37:37 +0100

    Minor adaptation

--- src/Acme/FrontendModule/FrontendModule.php
+++ src/Acme/FrontendModule/FrontendModule.php
@@ -4,17 +4,10 @@ namespace Acme\FrontendModule;
 
 use Acme\Blog\Article\ArticleRepository;
 use Interop\Framework\Module;
-use Interop\Framework\ModuleInterface;
-use Interop\Container\ContainerInterface;
 use Interop\Framework\Silex\AbstractSilexModule;
-use Interop\Framework\Silex\SilexFrameworkModule;
 use Psr\Log\LoggerInterface;
 use Silex\Application;
 use Silex\Provider\TwigServiceProvider;
-use Stack\UrlMap;
-use Mouf\StackPhp\SilexMiddleware;
-use Interop\Framework\HttpModuleInterface;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 /**
  * The frontend module is a Silex application.
@@ -43,10 +36,10 @@ class FrontendModule extends AbstractSilexModule
 		// Home
 		$app->get('/', function (Application $app) {
 			/** @var ArticleRepository $articleRepository */
-			$articleRepository = $this->rootContainer->get(ArticleRepository::class);
+			$articleRepository = $this->getRootContainer()->get(ArticleRepository::class);
 			$count = count($articleRepository->getAll());
 
-			$this->rootContainer->get(LoggerInterface::class)->debug('Someone was on the home page');
+			$this->getRootContainer()->get(LoggerInterface::class)->debug('Someone was on the home page');
 
 			return $app['twig']->render('home.twig', ['articleCount' => $count]);
 		});
